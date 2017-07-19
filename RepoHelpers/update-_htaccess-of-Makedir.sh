3a9baa45f5e5838448e4acaf67f9049474c0a05f
Martin Bless <martin@mbless.de>
2017-07-19 15:33:41 +0200

    Add RepoHelpers

---
+++ RepoHelpers/update-_htaccess-of-Makedir.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# update-_htaccess-of-Makedir.sh
+
+src=https://raw.githubusercontent.com/TYPO3-Documentation/typo3-docs-typo3-org-resources/master/userroot/scripts/config/_htaccess-2016-08.txt
+destdir="$PWD"/../ALL/Makedir
+dest=$destdir/_htaccess
+
+echo Updating "$dest"
+wget $src --output-document "$dest"
+
+chmod -R o+w $destdir
+
+echo Contents of
+echo "   $destdir"
+ls -la $destdir
