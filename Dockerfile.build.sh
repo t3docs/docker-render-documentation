7e84af14f00257d141a243b22d2f024b56b6c17e
Martin Bless <martin.bless@mbless.de>
2023-06-12 16:32:39 +0200

    Add option PLANTUML to Dockerfile.build.sh

--- Dockerfile.build.sh
+++ Dockerfile.build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-OUR_IMAGE_SHORT=${OUR_IMAGE_SHORT:-t3rd}
 OUR_IMAGE_TAG=${OUR_IMAGE_TAG:-develop}
+OUR_IMAGE_SHORT=${OUR_IMAGE_SHORT:-develop}
 EXITCODE=0
 
 function usage() {
@@ -15,6 +15,7 @@ function usage() {
    echo "   OUR_IMAGE_TAG=latest     OUR_IMAGE_SHORT=t3rd     /bin/bash  ./$(basename -- $0)"
    echo "   OUR_IMAGE_TAG=develop    OUR_IMAGE_SHORT=develop  /bin/bash  ./$(basename -- $0)"
    echo "   OUR_IMAGE_TAG=test       OUR_IMAGE_SHORT=test     /bin/bash  ./$(basename -- $0)"
+   echo "   OUR_IMAGE_TAG=test       OUR_IMAGE_SHORT=test     PLANTUML_JAR_VERSION=1.2023.6  /bin/bash  ./$(basename -- $0)"
    echo
 }
 
@@ -40,6 +41,9 @@ if ((1)); then
    if [[ ! -z "$OUR_IMAGE_TAG" ]]; then
      cmd="$cmd --build-arg OUR_IMAGE_TAG=\"${OUR_IMAGE_TAG}\""
    fi
+   if [[ ! -z "$PLANTUML_JAR_VERSION" ]]; then
+     cmd="$cmd --build-arg PLANTUML_JAR_VERSION=\"${PLANTUML_JAR_VERSION}\""
+   fi
    cmd="$cmd ."
    echo $cmd
    eval "$cmd"
@@ -57,5 +61,10 @@ if ((1)); then
    fi
    echo "building t3docs/render-documentation:${OUR_IMAGE_TAG} in $BUILD_ELAPSED seconds"
 fi
+echo
 echo Looking for image 't3docs:render-documentation:'"${OUR_IMAGE_TAG}"
 docker image ls | awk '$1=="t3docs/render-documentation" && $2=="'${OUR_IMAGE_TAG}'"'
+echo
+echo Hint:
+echo    Consider command \'docker builder prune\' to stop \'/var/lib/docker\' from growing
+echo
